a8783306a4ad624398e39a0dff91035fdc270161
briesbutler <gabriellastaffordbutler@gmail.com>
2022-07-27 20:29:33 -0500

    finished

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-let quote = document.getElementById("quote");
-let author = document.getElementById("author");
-let btn = document.getElementById("btn");
-const url = "https://api.quotable.io/random";
-let getQuote = () => {
-  fetch(url)
-    .then((data) => data.json())
-    .then((item) => {
-      quote.innerText = item.content;
-      author.innerText = item.author;
-    });
+const display = document.querySelector('.display');
+
+let keySelect = (k) => {
+  timesClicked = 0;
+  if (k === '+' || k === '×' || k === '÷') {
+    if (!display.innerText.includes('+') && !display.innerText.includes('×') && !display.innerText.includes('÷') && display.innerText.slice(-1) != '.' && display.innerText.slice(-1) != '-' && display.innerText != '0') {
+      display.innerText = display.innerText + k;
+    };
+  } else if (k === '-') {
+    if (display.innerText === '0') {
+      display.innerText = k;
+    } else if (display.innerText.endsWith('-') || display.innerText.split('-').length - 1 > 1 ) {
+      display.innerText = display.innerText;
+    } else {
+      display.innerText += k;
+    }
+  } else if (k === '.') {
+    if (display.innerText === '0') {
+      display.innerText = k;
+    } else if (display.innerText.split('.').length - 1 > 1 || display.innerText.endsWith('.') || (display.innerText.includes('.') && !display.innerText.includes('0-') && !display.innerText.includes('1-') && !display.innerText.includes('2-') && !display.innerText.includes('3-') && !display.innerText.includes('4-') && !display.innerText.includes('5-') && !display.innerText.includes('6-') && !display.innerText.includes('7-') && !display.innerText.includes('8-') && !display.innerText.includes('9-') && !display.innerText.includes('+') && !display.innerText.includes('×') && !display.innerText.includes('÷'))) { 
+      display.innerText = display.innerText;
+    } else {
+      display.innerText += k;
+    }
+  } else if (display.innerText === '0' || display.innerText === '_' || display.innerText === 'undefined') {
+    display.innerText = (k);
+  } else {
+    display.innerText = display.innerText + (k);
+  }
 };
-window.addEventListener("load", getQuote);
-btn.addEventListener("click", getQuote);
+
+let timesClicked = 0;
+let eqSelect = () => {
+  timesClicked++;
+  if (timesClicked > 1) {
+    display.innerText = '0'
+  } else {    
+    if (display.innerText.includes('×')) {
+      let x = display.innerText.replace('×', '*')
+      let y = eval(x)
+      display.innerText = y
+    } else if (display.innerText.includes('÷')) {
+      let x = display.innerText.replace('÷', '/');
+      let y = eval(x)
+      display.innerText = y
+    } else {
+      let x = display.innerText;
+      let y = eval(x)
+      display.innerText = y
+    }
+  }
+}  
